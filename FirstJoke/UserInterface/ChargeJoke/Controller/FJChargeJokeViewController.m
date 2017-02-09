c484d7d9c6efab860de5d3a6e1cf513fff82a37a
刘伟强 <718691587@qq.com>
2017-02-09 13:36:08 +0800

    添加内购代码

--- FirstJoke/UserInterface/ChargeJoke/Controller/FJChargeJokeViewController.m
+++ FirstJoke/UserInterface/ChargeJoke/Controller/FJChargeJokeViewController.m
@@ -11,12 +11,22 @@
 #import "FJChargeJokeTableViewCell.h"
 #import "LQFullscreenImageViewController.h"
 
+#import <StoreKit/StoreKit.h>
+
 #define chargeJokeTotalPageNumber @"chargeJokeTotalPageNumber"
 
-@interface FJChargeJokeViewController ()<UITableViewDelegate,UITableViewDataSource,FJChargeJokeTableViewCellDelegate>
+#define product1_ID @"com.LCTechnology.FirstJoke1"
+
+//沙盒测试环境验证
+#define SANDBOX @"https://sandbox.itunes.apple.com/verifyReceipt"
+//正式环境验证
+#define AppStore @"https://buy.itunes.apple.com/verifyReceipt"
+
+@interface FJChargeJokeViewController ()<UITableViewDelegate,UITableViewDataSource,FJChargeJokeTableViewCellDelegate,SKPaymentTransactionObserver,SKProductsRequestDelegate>
 @property (strong, nonatomic) UITableView *chargeJokeTableView;
 @property (strong, nonatomic) NSMutableArray *dataArr;
 @property (nonatomic) NSInteger pageNumber;
+
 @end
 
 @implementation FJChargeJokeViewController
@@ -30,12 +40,12 @@
     
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"购买" style:UIBarButtonItemStylePlain target:self action:@selector(rightBarButtonItemFun)];
     
+     [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    
     // Do any additional setup after loading the view.
 }
 
 - (void)update:(NSInteger )pagenumber{
-
-
     [FJChargeJokeModel getjokeListOfPageNum:pagenumber Success:^(NSMutableArray *dataArr) {
         self.dataArr = dataArr;
         [self.chargeJokeTableView reloadData];
@@ -92,7 +102,133 @@
 }
 
 -(void)rightBarButtonItemFun{
-    [self update:++self.pageNumber];
+    NSString *product = product1_ID;
+    if([SKPaymentQueue canMakePayments]){
+        [self requestProductData:product];
+    }else{
+        NSLog(@"不允许程序内付费");
+    }
 }
 
+//请求商品
+- (void)requestProductData:(NSString *)type{
+    NSLog(@"-------------请求对应的产品信息----------------");
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleDark];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showWithStatus:nil];
+    
+    NSArray *product = [[NSArray alloc] initWithObjects:type,nil];
+    NSSet *nsset = [NSSet setWithArray:product];
+    SKProductsRequest *request = [[SKProductsRequest alloc] initWithProductIdentifiers:nsset];
+    request.delegate = self;
+    [request start];
+    
+}
+
+//收到产品返回信息
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response{
+    NSLog(@"--------------收到产品反馈消息---------------------");
+    NSArray *product = response.products;
+    if([product count] == 0){
+        [SVProgressHUD dismiss];
+        NSLog(@"--------------没有商品------------------");
+        return;
+    }
+    
+    NSLog(@"productID:%@", response.invalidProductIdentifiers);
+    NSLog(@"产品付费数量:%lu",(unsigned long)[product count]);
+    
+    SKProduct *p = nil;
+    for (SKProduct *pro in product) {
+        NSLog(@"%@", [pro description]);
+        NSLog(@"%@", [pro localizedTitle]);
+        NSLog(@"%@", [pro localizedDescription]);
+        NSLog(@"%@", [pro price]);
+        NSLog(@"%@", [pro productIdentifier]);
+        if([pro.productIdentifier isEqualToString:product1_ID]){
+            p = pro;
+        }
+    }
+    SKPayment *payment = [SKPayment paymentWithProduct:p];
+    NSLog(@"发送购买请求");
+    [[SKPaymentQueue defaultQueue] addPayment:payment];
+}
+
+//请求失败
+- (void)request:(SKRequest *)request didFailWithError:(NSError *)error{
+    [SVProgressHUD showErrorWithStatus:@"支付失败"];
+    NSLog(@"------------------错误-----------------:%@", error);
+}
+
+- (void)requestDidFinish:(SKRequest *)request{
+    [SVProgressHUD dismiss];
+    NSLog(@"------------反馈信息结束-----------------");
+}
+
+/**
+ *  验证购买，避免越狱软件模拟苹果请求达到非法购买问题
+ *
+ */
+-(void)verifyPurchaseWithPaymentTransaction{
+    //从沙盒中获取交易凭证并且拼接成请求体数据
+    NSURL *receiptUrl=[[NSBundle mainBundle] appStoreReceiptURL];
+    NSData *receiptData=[NSData dataWithContentsOfURL:receiptUrl];
+    NSString *receiptString=[receiptData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];//转化为base64字符串
+    NSString *bodyString = [NSString stringWithFormat:@"{\"receipt-data\" : \"%@\"}", receiptString];//拼接请求数据
+    NSData *bodyData = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
+    //创建请求到苹果官方进行购买验证
+    NSURL *url=[NSURL URLWithString:SANDBOX];
+    NSMutableURLRequest *requestM=[NSMutableURLRequest requestWithURL:url];
+    requestM.HTTPBody=bodyData;
+    requestM.HTTPMethod=@"POST";
+    //创建连接并发送同步请求
+    NSError *error=nil;
+    NSData *responseData=[NSURLConnection sendSynchronousRequest:requestM returningResponse:nil error:&error];
+    if (error) {
+        NSLog(@"验证购买过程中发生错误，错误信息：%@",error.localizedDescription);
+        return;
+    }
+    NSDictionary *dic=[NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:nil];
+    if([dic[@"status"] intValue]==0){
+        NSLog(@"购买成功！");
+        [self update:++self.pageNumber];
+        //在此处对购买记录进行存储，可以存储到开发商的服务器端
+    }else{
+        NSLog(@"购买失败，未通过验证！");
+    }
+}
+//监听购买结果
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transaction{
+    
+    for(SKPaymentTransaction *tran in transaction){
+        switch (tran.transactionState) {
+            case SKPaymentTransactionStatePurchased:{
+                NSLog(@"交易完成");
+                [self verifyPurchaseWithPaymentTransaction];
+                [[SKPaymentQueue defaultQueue] finishTransaction:tran];
+            }
+                break;
+            case SKPaymentTransactionStatePurchasing:
+                NSLog(@"商品添加进列表");
+                break;
+            case SKPaymentTransactionStateRestored:{
+                NSLog(@"已经购买过商品");
+                [[SKPaymentQueue defaultQueue] finishTransaction:tran];
+            }
+                break;
+            case SKPaymentTransactionStateFailed:{
+                [[SKPaymentQueue defaultQueue] finishTransaction:tran];
+                [SVProgressHUD setMinimumDismissTimeInterval:1];
+                [SVProgressHUD showErrorWithStatus:@"购买失败"];
+            }
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+- (void)dealloc{
+    [[SKPaymentQueue defaultQueue] removeTransactionObserver:self];
+}
 @end
